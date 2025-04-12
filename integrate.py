43ca6a0c41f8aafdaeed6e68e745716424253a29
ZaminAhammed <zaminahammed801@gmail.com>
2025-04-12 18:13:28 +0530

    Delete integrate.py

--- integrate.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd;
-import numpy as np;
-
-pf = pd.read_csv("pf_haploatlas.csv");
-ge = pd.read_csv("gene_essentiality.csv");
-
-final = pd.merge(ge,pf,on='Gene',how="outer");
-final.to_csv("integrated.csv",index=False);
